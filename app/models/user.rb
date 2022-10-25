b545d2698ebd74af799421bd654513bf582537d2
kaityi <114052405+kaityi@users.noreply.github.com>
2022-10-25 14:57:18 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name2, presence: true
-  validates :last_name_2, presence: true
   validates :date_of_birth, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
@@ -19,7 +15,7 @@ class User < ApplicationRecord
     validates :last_name_2
   end
 
-  validates :nickname, presence: true
+  
 
 
 end
