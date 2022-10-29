4e7fff07f5808831e716360aed77e0c916745e74
kaityi <114052405+kaityi@users.noreply.github.com>
2022-10-29 19:02:58 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :find_item, only: [:show, :edit, :update]
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
   
 
 
@@ -26,10 +26,9 @@ class ItemsController < ApplicationController
    
   end
   def edit
-    
-      if  current_user.id != @item.user.id 
-        redirect_to root_path
-      end
+    if current_user.id != @item.user.id 
+      redirect_to root_path
+   end
   end
   def update
     
@@ -38,6 +37,10 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+  def destroy
+     @item.destroy
+     redirect_to root_path
   end
   private
 
